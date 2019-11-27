4df0e78a71ecf73adb0f65338047c4d9ed2e9138
Boris Sulicenko <b.sulicenko@gmail.com>
2019-11-27 18:41:41 +0100

    Implementirani integracioni i unit testovi za PricingServiceImpl. Obrisane su nepotrebne metode iz PricingRepository.

--- ktsnwt-service/src/test/java/rs/ac/uns/ftn/ktsnwt/constants/PricingConstants.java
+++ ktsnwt-service/src/test/java/rs/ac/uns/ftn/ktsnwt/constants/PricingConstants.java
@@ -1,5 +1,10 @@
 package rs.ac.uns.ftn.ktsnwt.constants;
 
+import rs.ac.uns.ftn.ktsnwt.model.Pricing;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PricingConstants {
 
     public static final int DB_NUM_PRICINGS = 2;
@@ -15,8 +20,26 @@ public class PricingConstants {
     public static final Long DB_REFERENCED_EVENT_DAY_ID_2 = 1L;
     public static final Long DB_REFERENCED_SECTOR_ID_2 = 2L;
 
+    // Mocking values
+    public static final int NUM_MOCKED_PRICINGS = 2;
+    public static final Long MOCK_ID_1 = 9999L;
+    public static final Long MOCK_ID_2 = 99999L;
+
     // Non existing
     public static final Long NON_EXISTING_DB_ID = 123456L;
+
+    public static List<Pricing> createPricingListForMocking() {
+        Pricing pricing1 = new Pricing();
+        pricing1.setId(MOCK_ID_1);
+        Pricing pricing2 = new Pricing();
+        pricing2.setId(MOCK_ID_2);
+
+        List<Pricing> pricings = new ArrayList<>();
+        pricings.add(pricing1);
+        pricings.add(pricing2);
+
+        return pricings;
+    }
 
     private PricingConstants() {
     }
--- ktsnwt-service/src/test/java/rs/ac/uns/ftn/ktsnwt/service/suites/ServiceIntegrationTests.java
+++ ktsnwt-service/src/test/java/rs/ac/uns/ftn/ktsnwt/service/suites/ServiceIntegrationTests.java
@@ -3,10 +3,12 @@ package rs.ac.uns.ftn.ktsnwt.service.suites;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import rs.ac.uns.ftn.ktsnwt.service.address.AddressServiceImplIntegrationTest;
+import rs.ac.uns.ftn.ktsnwt.service.pricing.PricingServiceImplIntegrationTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        AddressServiceImplIntegrationTest.class
+        AddressServiceImplIntegrationTest.class,
+        PricingServiceImplIntegrationTest.class
 })
 public class ServiceIntegrationTests {
 }
