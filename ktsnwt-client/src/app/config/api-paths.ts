d4ecbc8d530216f422a168e45c9a6a74fb14232c
Boris Sulicenko <b.sulicenko@gmail.com>
2019-12-09 18:18:53 +0100

    Napravljena pocetna verzija home page-a.

--- ktsnwt-client/src/app/config/api-paths.ts
+++ ktsnwt-client/src/app/config/api-paths.ts
@@ -7,3 +7,4 @@ export const API_MY_PROFILE = API_BASE + '/api/users/my-profile';
 export const API_USER_PASSWORD_CHANGE = API_BASE + '/auth/change-password';
 export const API_USER_IMAGE = API_BASE + '/api/images/users';
 export const API_LOCATIONS_ALL = API_BASE + "/api/locations/all";
+export const API_EVENTS = API_BASE + '/api/event/all'; // TODO: ADD LATER /public/
