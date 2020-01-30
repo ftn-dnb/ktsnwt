6d35100c690bc91e6ce724af62aa31b48afb3ad8
boris-ns <b.sulicenko@gmail.com>
2020-01-30 18:01:31 +0100

    Implementirano je otkazivanje rezervacije

--- ktsnwt-client/src/app/services/ticket.service.ts
+++ ktsnwt-client/src/app/services/ticket.service.ts
@@ -1,6 +1,6 @@
-import { API_BUY_TICKET } from './../config/api-paths';
+import { API_BUY_TICKET, API_CANCEL_TICKET, API_MY_RESERVATIONS } from './../config/api-paths';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -13,5 +13,17 @@ export class TicketService {
 
   buyTicket(id: number): Observable<any> {
     return this.http.get(`${API_BUY_TICKET}/${id}`);
+  }
+
+  cancelTicket(id: number): Observable<any> {
+    return this.http.delete(`${API_CANCEL_TICKET}/${id}`);
+  }
+
+  getReservationsOnePage(pageNum: number, pageSize: number): Observable<any>  {
+    return this.http.get(API_MY_RESERVATIONS, {
+      params: new HttpParams()
+        .set('page', pageNum.toString())
+        .set('size', pageSize.toString())
+    });
   }
 }
